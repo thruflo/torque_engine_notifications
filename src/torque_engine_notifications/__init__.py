ee947419c2290076078c46e3fd2f9df95aa02274
James Arthur <thruflo@gmail.com>
2016-06-15 17:03:05 +0100

    __init__: provide a top level pyramid includeme function.

--- src/torque_engine_notifications/__init__.py
+++ src/torque_engine_notifications/__init__.py
@@ -1,1 +1,21 @@
-# Having an ``__init__.py`` file in it makes a directory a Python package.
+# -*- coding: utf-8 -*-
+
+"""Provides a Pyramid configuration entry point to register the notification
+  configuration directives, provide the `request.notifications` dispatch api
+  and expose the work engine `/notify` route.
+
+  I.e.: everything that's needed to enable the notification system.
+"""
+
+from . import config
+from . import dispatch
+from . import view
+
+def includeme(config):
+    modules = (
+        config,
+        dispatch,
+        view,
+    )
+    for x in modules:
+        config.include(x)
